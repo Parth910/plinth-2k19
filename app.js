3a31f349091b933285c42c91972fae41fb476efe
nimbus1212 <swarnim.1212@gmail.com>
2018-10-07 15:38:42 +0530

    basic server setup

--- app.js
+++ app.js
@@ -5,26 +5,29 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
 
+var routes = require('./routes/index');
 
 var app = express();
-app.listen(process.env.PORT || 3000);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
+
 app.use('*/js', express.static(path.join(__dirname, 'public/js')))
 app.use('*/css', express.static(path.join(__dirname, 'public/css')))
 app.use('*/media', express.static(path.join(__dirname, 'public/media')))
 
-app.use(bodyParser.json());
+app.use(logger('dev'));
 app.use(favicon(path.join(__dirname, 'public/media/fav', 'favicon.ico')));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
 
 
 /// catch 404 and forwarding to error handler
@@ -40,11 +43,11 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error_dev', {
-			message: err.message,
-			error: err
-		  });
+	  res.status(err.status || 500);
+	  res.render('error_dev', {
+		message: err.message,
+		error: err
+	  });
 	});
 } else {
 	// production error handler
@@ -53,6 +56,10 @@ if (app.get('env') === 'development') {
 		res.status(err.status || 500);
 		if(err.status === 401){
 			res.end(err.message);
+		  //   res.render('error_401', {
+		  //     message: err.message,
+		  //     "isLoggedIn" : isLoggedIn,
+		  //   });
 		}
 		res.redirect('/');
 	});
