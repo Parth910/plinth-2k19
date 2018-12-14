3e69c089208e1b95e092a86b81b9887c538f20ef
nimbus1212 <swarnim.1212@gmail.com>
2018-12-15 00:56:20 +0530

    minor change

--- public/js/mun.js
+++ public/js/mun.js
@@ -21,7 +21,8 @@ $('input[type="radio"]').click(function () {
             .end()
             .append('<option value="DISEC">DISEC</option>' +
                 '<option value="UNHRC">UNHRC</option>' +
-                '<option value="LOK SABHA">LOK SABHA</option>'); 
+                '<option value="LOK SABHA">LOK SABHA</option>' +
+                '<option value="The Stakeholders Meet">The Stakeholders Meet</option>'); 
 
         $('#amount').text('Amount :₹ 1500/-');
         $('#hidden-matter').show();
