bc3444d5581bc36671095ed92d866261166eb7e8
nimbus1212 <swarnim.1212@gmail.com>
2018-12-21 20:03:22 +0530

    cryptex admin dashboard

--- public/js/cryptex.js
+++ public/js/cryptex.js
@@ -62,3 +62,18 @@ $(document).ready(function () {
     }).fadeIn(400);
   }, 6000);
 });
+$('.toggle').click(function (e) {
+  e.preventDefault();
+
+  var $this = $(this);
+
+  if ($this.next().hasClass('show')) {
+    $this.next().removeClass('show');
+    $this.next().slideUp(350);
+  } else {
+    $this.parent().parent().find('li .inner').removeClass('show');
+    $this.parent().parent().find('li .inner').slideUp(350);
+    $this.next().toggleClass('show');
+    $this.next().slideToggle(350);
+  }
+});
